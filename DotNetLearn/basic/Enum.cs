fc3c5c3ec726e9fbeec18abab2745c9aa1ec8014
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-10 23:39:00 +0530

    update

---
+++ DotNetLearn/basic/Enum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DotNetLearn.basic
+{
+    class Enum
+    {
+        enum Days { Sun, Mon, };
+        static void Main(string[] args)
+        {
+           
+        int w = (int)Days.Mon;
+            Console.WriteLine(w);
+
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNetLearn/basic/StructExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DotNetLearn.basic
+{
+
+
+    public struct Books
+    {
+        int price;
+        string aName;
+        public Books(int p, string n)
+        {
+            price = p;
+            aName = n;
+        }
+        public int totalPrice(int c)
+        {
+            return c * price;
+        }
+    }
+    class StructExample
+    {
+        static void Main(string[] args)
+        {
+            Books b = new Books(120, "pradeep");
+           int tp= b.totalPrice(5);
+
+
+            //Console.WriteLine("Author Name - " + b.aName);
+            //Console.WriteLine(" Price - " +b.price);
+
+            Console.WriteLine("Total Price - "+tp);
+
+
+            DateTime dt = new DateTime(2015, 12, 31);
+            Console.WriteLine("Date- " + dt.Date);
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
