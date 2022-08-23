971cde57883d9cf7770bea4c263294abda5cb3a5
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-24 03:04:58 +0530

    exception handling

---
+++ DotNetLearn/basic/ExceptionHandlingExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DotNetLearn.basic
+{
+    class ExceptionHandlingExample
+    {
+
+        static void Main(string[] arg)
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+                // int x = a / b;
+
+                if (a <= 0 && b <= 0)
+                {
+                    throw new Exception("Both is less then Zero");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finlly");
+            }
+        
+
+            Console.ReadLine();
+        }
+    }
+}
