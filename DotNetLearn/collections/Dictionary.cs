03e0c41048bfb325f1216bd5067fb8d7fc7225dd
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-25 00:09:50 +0530

    collection

---
+++ DotNetLearn/collections/Dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetLearn.collections
+{
+    class Dictionary
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            names.Add("1", "Sonoo");
+            names.Add("2", "Peter");
+            names.Add("3", "James");
+            names.Add("4", "Ratan");
+            names.Add("5", "Irfan");
+
+            foreach (KeyValuePair<string, string> kv in names)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
