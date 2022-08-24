d6d473116b816b84e3855084523f20282d40a7f8
pradeep <pradeepmaurya.cse@gmail.com>
2022-08-24 23:33:54 +0530

    generic

---
+++ DotNetLearn/basic/GenericType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLearn.basic
+{
+    class GenericType
+    {
+        static void Main(string[] args)
+        {
+            MyClas<int, string> obj = new MyClas<int, string>();
+            obj.property = 100;
+            obj.behaver(100, "pradeep");
+            Console.ReadLine();
+        }
+    }
+
+    class MyClas<T1, T2>
+    {
+        public T1 property { get; set; }
+        public void behaver(T1 p, T2 q)
+        {
+            Console.WriteLine(p + "  " + q);
+        }
+    }
+}
---
+++ DotNetLearn/windowsform-mysql/StudentNew.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace DotNetLearn.windowsform_mysql
+{
+    public partial class StudentNew : Form
+    {
+        public StudentNew()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MySqlConnection cn = new MySqlConnection("server=localhost;database=sakila;User ID=root;Password=cdac123");
+
+            //cn.Open();
+
+            //MySqlCommand cmd = new MySqlCommand("Select * from actor;", cn);
+
+            //MySqlDataReader rdr = cmd.ExecuteReader();
+            //BindingSource source = new BindingSource();
+            //source.DataSource = rdr;
+
+            //dataGridView1.DataSource = source;
+            //// dataGridView1.DataBind();
+            //cn.Close();
+        }
+    }
+}
