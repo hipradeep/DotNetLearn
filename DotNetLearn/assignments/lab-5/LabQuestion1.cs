2320ac69d41e85c9a0e41a5d31690dc0b1e952f5
pradeep <pradeepmaurya.cse@gmail.com>
2022-09-29 20:49:41 +0530

    lab

---
+++ DotNetLearn/assignments/lab-5/LabQuestion1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLearn.assignments.lab_5
+{
+    class LabQuestion1
+    {
+        static void Main(string[] arg)
+        {
+            Rectangle r = new Rectangle(12, 12);
+        
+            r.displayArea();
+
+        }
+
+    }
+
+    abstract class Shape
+    {
+     
+
+        private  string color;
+        public string Color   // property
+        {
+            get { return color; }   // get method
+            set { color = value; }  // set method
+        }
+        public abstract double Area();
+
+    }
+    class Rectangle : Shape
+    {
+        private int Length;
+        private int Width;
+
+        public Rectangle(int l, int w)
+        {
+            Length = l;
+            Width = w;
+        }
+
+        public override double Area()
+        {
+            return Length * Width ;
+        }
+        public  void displayArea()
+        {
+            Console.WriteLine("Area is" + this.Area());
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
